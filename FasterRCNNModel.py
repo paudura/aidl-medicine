d97061198d62b0336a88f155bfb12e5c6928e15a
Pau <pau.dura17@gmail.com>
2021-06-19 21:46:50 +0000

    commit pau

---
+++ FasterRCNNModel.py
@@ -0,0 +1,97 @@
+# RUNNING THE MODEL
+import torch
+import torchvision
+import torch.optim as optim
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import  FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+
+
+import data_input as data_input
+
+pneumonia_train_dataset = data_input.pneumoniaDataset(train = True)
+pneumonia_test_dataset = data_input.pneumoniaDataset(train = False)
+
+#pneumonia_train_dataset[4][0].shape
+
+
+def collate_fn(batch):
+    images = []
+    targets = []
+    for i, t in batch:
+        images.append(i)
+        targets.append(t)
+    return images, targets
+
+pneumonia_trainloader = torch.utils.data.DataLoader(pneumonia_train_dataset, batch_size=3, num_workers=0,collate_fn=collate_fn)
+pneumonia_testloader = torch.utils.data.DataLoader(pneumonia_test_dataset, batch_size=1, num_workers=0, collate_fn=collate_fn)
+
+
+#FasterRCNNModel
+
+
+if not torch.cuda.is_available():
+    raise RuntimeError("You should enable GPU runtime.")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True, min_size = 1024)
+num_classes = 2
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+model = model.train().to(device)
+optimizer = optim.Adam(model.parameters(), weight_decay=0.0001)
+
+# Versio Ines
+#Model train
+#model.train()
+#for images, targets in enumerate(pneumonia_trainloader):
+#    #print(images)
+#    optimizer.zero_grad()
+#    predictions, loss_dict = model(images, targets)
+#    loss = sum(loss for loss in loss_dict.values())
+#    loss.backward()
+
+#    if i%2 == 0:
+#        loss_dict_printable = {k: f"{v.item():.2f}" for k, v in loss_dict.items()}
+#        print(f"[{i}/{len(pneumonia_trainloader)}] loss: {loss_dict_printable}")
+
+
+
+# Versio pau v1
+#for i, (images, targets) in enumerate(pneumonia_trainloader):
+#    print (i)
+#    optimizer.zero_grad()
+#    print("optimizer zero grad OK")
+#    predictions, loss_dict = model(images, targets)
+#    print("model OK")
+#    loss = sum(loss for loss in loss_dict.values())
+#    print("Loss OK")
+#    loss.backward()
+#    print("Loss OK2")
+#    optimizer.step()
+#    print("optimizer OK")
+
+# Versio pau V2: FUNCIONA!
+
+model.train()
+for batch_idx, sample in enumerate(pneumonia_trainloader):
+    #print(batch_idx)
+    images, targets = sample[0], sample[1]
+    #print("OK1")
+    optimizer.zero_grad()
+    #print("OK2")
+    loss_dict = model(images, targets)
+    #print("OK3")
+    loss = sum(loss for loss in loss_dict.values())
+    #print("OK4")
+    loss.backward()
+    #print("OK5")
+    optimizer.step()
+    if batch_idx%2 == 0:
+        loss_dict_printable = {k: f"{v.item():.2f}" for k, v in loss_dict.items()}
+        print(f"[{batch_idx}/{len(pneumonia_trainloader)}] loss: {loss_dict_printable}")
+
+
+
--- data_input.py
+++ data_input.py
@@ -3,7 +3,15 @@ import numpy
 import csv
 import os 
 from glob import glob
-
+import json
+from torchvision.transforms.functional import to_tensor
+from PIL import Image, ImageDraw
+import torch
+import pydicom
+import numpy as np
+import copy
+import random
+torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ################
 ## INPUT DATA ##
@@ -47,16 +55,14 @@ def train_dataframe():
 
         image_df = pd.DataFrame({'path': glob(os.path.join(dicom_dir, '*.dcm'))})
         image_df['patientId'] = image_df['path'].map(lambda x: os.path.splitext(os.path.basename(x))[0])
-        print(image_df.shape[0], 'images found')
+        #print(image_df.shape[0], 'images found')
         img_pat_ids = set(image_df['patientId'].values.tolist())
         box_pat_ids = set(comb_box_df['patientId'].values.tolist())
         # check to make sure there is no funny business
         #assert img_pat_ids.union(box_pat_ids)==img_pat_ids, "Patient IDs should be the same"
-        image_bbox_df = pd.merge(comb_box_df, 
-                         image_df, 
-                         on='patientId',
-                        how='left').sort_values('patientId')
-        print(image_bbox_df.shape[0], 'image bounding boxes')
+        image_bbox_df = pd.merge(comb_box_df, image_df, on='patientId', how='left').sort_values('patientId')
+        #print(image_bbox_df.shape[0], 'image bounding boxes')
+        #print(sum(image_bbox_df["path"].isna()))
         return(image_bbox_df)
 
 def test_dataframe():
@@ -67,10 +73,105 @@ def test_dataframe():
         return(image_df)
 
 
-#dd = initial_dataframe()
+#dd = train_dataframe()
 #print(dd.head(n =10))
 #print(dd.columns)
 #print(dd.shape)
 
+
+
+class pneumoniaDataset(object):
+    def __init__(self, train = True, number_validation = 20, device = "cpu"):
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.train = train
+        self.number_validation = number_validation
+        dd = train_dataframe()
+        dd = dd.loc[~dd['path'].isna(),:]
+        dd.reset_index(drop = True, inplace = True)
+        dd = dd.loc[dd["Target"] == 1, :]
+        dd.reset_index(drop = True, inplace = True)
+        #random.shuffle(dd)             
+
+        if self.train:
+            dd = dd.loc[number_validation:]
+            dd.reset_index(drop = True, inplace = True)
+        else:
+            # create validation dataset
+            dd = dd.loc[:number_validation]
+            dd.reset_index(drop = True, inplace = True)
+
+        self.imgs = []
+        self.annotations = []
+
+        for i in range(dd.shape[0]):
+            filename = dd.loc[i, "path"] 
+            self.imgs.append(filename)
+            self.annotations.append(dd.loc[i, ["x", "y", "height", "width"]])
+
+
+    def __getitem__(self, idx, device = "cpu"):
+        # load images
+        img_path = self.imgs[idx]
+        ds = pydicom.read_file(img_path) #no llegeix 
+        image = ds.pixel_array
+        # If grayscale. Convert to RGB for consistency.
+        if len(image.shape) != 3 or image.shape[2] != 3:
+            image = np.stack((image,) * 3, -1)
+
+        # get bounding box coordinates for each mask
+        boxes = []
+        #for _, annotation in self.annotations[idx]:
+        x  = self.annotations[idx]["x"]
+        y = self.annotations[idx]["y"]
+        width = self.annotations[idx]["width"]
+        heigth = self.annotations[idx]["height"]
+            
+        if np.isnan(x):
+            px = []
+            py = []
+        else:
+            px = [x, x - width/2, x + width/2]
+            py = [y, y - heigth/2, y + heigth/2]
+
+        poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)] #not necessary
+        poly = [p for x in poly for p in x]
+            
+        if np.isnan(x):
+            boxes.append([])
+        else:
+            boxes.append([min(px), min(py), max(px), max(py)]) 
+        #select the corners of the boxes for each axis. it should be a list with 4 values: 2 coordinates.
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32, device = device)
+        # there is only one class
+        labels = torch.ones((len(boxes),), dtype=torch.int64, device = device)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+
+        image = to_tensor(image).to(device)
+        
+        return image, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+
+#pneumonia_train_dataset = pneumoniaDataset(train = True)
+#pneumonia_test_dataset = pneumoniaDataset(train = False)
+
+#def collate_fn(batch):
+#    images = []
+#    targets = []
+#    for i, t in batch:
+#        images.append(i)
+#        targets.append(t)
+#    return images, targets
+
+#pneumonia_trainloader = torch.utils.data.DataLoader(pneumonia_train_dataset, batch_size=4, num_workers=0,collate_fn=collate_fn)
+#pneumonia_testloader = torch.utils.data.DataLoader(pneumonia_test_dataset, batch_size=1, num_workers=0, collate_fn=collate_fn)
 
 
