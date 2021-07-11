348937b45b952cc8f2181e0682128621d5fe0c3c
Pau <pau.dura17@gmail.com>
2021-07-11 19:26:18 +0000

    pau commit

--- auxiliar.py
+++ auxiliar.py
@@ -17,9 +17,9 @@ def collate_fn(batch):
 def bb_intersection_over_union(boxA, boxB):
     	# determine the (x, y)-coordinates of the intersection rectangle
 	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[0], boxB[1])
-	xB = min(boxA[0], boxB[2])
-	yB = min(boxA[0], boxB[3])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
 	# compute the area of intersection rectangle
 	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
 	# compute the area of both the prediction and ground-truth
